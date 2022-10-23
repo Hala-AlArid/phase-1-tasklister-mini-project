63963fc4f6e033f0533fb031fbdca15faae0b82b
Hala AlArid <halaalarid99@gmail.com>
2022-10-24 02:21:50 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const tasks = [];
+  document.getElementById("tasks").innerHTML = tasks;
+
+  function addTask(event){
+    var x = document.getElementById("new-task-description").value;
+    tasks.push(x);
+
+    document.getElementById("tasks").innerHTML = tasks;
+
+    event.preventDefault();
+  }
+  
+  const form = document.getElementById("create-task-form");
+  
+  form.addEventListener('submit', addTask);
+  
 });
+
+
+
+
+
